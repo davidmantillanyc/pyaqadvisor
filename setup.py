d8e938f89ec48478027c571d0202a3d293c8a200
David Mantilla <davidmantillanyc@gmail.com>
2016-10-16 03:22:28 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-        version=0.1,
+        version='0.2.dev0',
         name="pyaqadvisor",
         package_dir={'pyaqadvisor': 'pyaqadvisor'},
         packages=find_packages(),
